856f4b99003a159e49c70fbbbed445edaf7b4350
chynnalew <chynnalew@yahoo.com>
2021-10-29 14:35:39 -0700

    add home index, path in home controller, shared layout view

---
+++ LocalBusinessClient/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace LocalBusinessClient.Models
+{
+  public class ErrorViewModel
+  {
+    public string RequestId { get; set; }
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
